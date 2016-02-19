654e8d076961e6ecaad7ee76f95e52f1eef21806
Jesse Vincent <jesse@bestpractical.com>
2016-02-19 15:36:52 -0800

    Switch our cRGB struct to be the same as what light_ws2812 uses

--- KeyboardioScanner.h
+++ KeyboardioScanner.h
@@ -3,14 +3,11 @@
 #include <Arduino.h>
 
 
-typedef union {
-    struct {
-        uint8_t b;
-        uint8_t g;
-        uint8_t r;
-    };
-    uint8_t array[3];
-} cRGB;
+struct cRGB {
+    uint8_t b;
+    uint8_t g;
+    uint8_t r;
+};
 
 
 #define TWI_CMD_NONE 0x00
