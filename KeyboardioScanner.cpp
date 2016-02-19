87b0796711f9de86f4c9702252012f71dd5427d3
Jesse Vincent <jesse@bestpractical.com>
2016-02-19 15:35:23 -0800

    astyle

--- KeyboardioScanner.cpp
+++ KeyboardioScanner.cpp
@@ -101,7 +101,7 @@ void KeyboardioScanner::sendLEDBank(byte bank) {
     Wire.beginTransmission(addr);
     Wire.write(TWI_CMD_LED_BASE+bank);
     for (int i=0; i<LED_BYTES_PER_BANK; i++) {
-            Wire.write(ledData.bytes[bank][i]);
+        Wire.write(ledData.bytes[bank][i]);
     }
     Wire.endTransmission();
 }
--- examples/KeyboardExample/KeyboardExample.ino
+++ examples/KeyboardExample/KeyboardExample.ino
@@ -4,26 +4,26 @@
 KeyboardioScanner controller(0);
 
 void setup() {
-  Serial.begin(9600);
-  Wire.begin();
+    Serial.begin(9600);
+    Wire.begin();
 }
 
 void loop() {
 
-  // check if a key is ready, and if so, then read it.
-  key_t k = controller.readKey();
-  if (k.key>0) {
-    Serial.print("Controller ");
-    Serial.print(k.ad01, DEC);
-    if (k.down) {
-      Serial.print("  pressed key ");
-    } else {
-      Serial.print(" released key ");
+    // check if a key is ready, and if so, then read it.
+    key_t k = controller.readKey();
+    if (k.key>0) {
+        Serial.print("Controller ");
+        Serial.print(k.ad01, DEC);
+        if (k.down) {
+            Serial.print("  pressed key ");
+        } else {
+            Serial.print(" released key ");
 
+        }
+        Serial.print(k.key, BIN);
+        Serial.print("\n");
     }
-    Serial.print(k.key, BIN);
-    Serial.print("\n");
-  }
 
-  delay(1);
+    delay(1);
 }
