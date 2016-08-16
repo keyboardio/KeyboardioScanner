9810df50b2f66da02510707126181982692a41b8
Jesse Vincent <jesse@bestpractical.com>
2016-08-15 18:25:43 -0700

    Add client code for 'check the attiny's version'

---
+++ examples/VersionExample/VersionExample.ino
@@ -0,0 +1,22 @@
+#include <Wire.h>
+#include <KeyboardioScanner.h>
+
+KeyboardioScanner controller(0);
+
+void setup() {
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
+  pinMode(7, OUTPUT);
+  digitalWrite(7, LOW);
+
+  Serial.begin(9600);
+  Wire.begin();
+  delay(5000);
+  Serial.print("Talking to an ATTiny running scanner firmware version ");
+    Serial.print(controller.readVersion());
+    Serial.println();
+}
+
+void loop() {
+    1;
+}
