beb8b51a0b7b1bb8bee56983993470f09e482092
Jesse Vincent <jesse@keyboard.io>
2017-05-09 13:29:47 -0700

    remove double-gamma correction in the rainbow wave example

--- examples/RainbowWaveExample/RainbowWaveExample.ino
+++ examples/RainbowWaveExample/RainbowWaveExample.ino
@@ -2,33 +2,10 @@
 
 cRGB black;
 cRGB rainbow;
-cRGB rainbow_out;
 int rainbow_current_ticks = 0;
 int rainbow_wave_ticks = 1;
 uint16_t rainbow_hue = 0;   //stores 0 to 614
-
 uint16_t rainbow_wave_hue = 0;   //stores 0 to 614
-
-const uint8_t PROGMEM gamma[] = {
-    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-    0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
-    1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  2,  2,
-    2,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,  4,  4,  5,  5,  5,
-    5,  6,  6,  6,  6,  7,  7,  7,  7,  8,  8,  8,  9,  9,  9, 10,
-    10, 10, 11, 11, 11, 12, 12, 13, 13, 13, 14, 14, 15, 15, 16, 16,
-    17, 17, 18, 18, 19, 19, 20, 20, 21, 21, 22, 22, 23, 24, 24, 25,
-    25, 26, 27, 27, 28, 29, 29, 30, 31, 32, 32, 33, 34, 35, 35, 36,
-    37, 38, 39, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 50,
-    51, 52, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 66, 67, 68,
-    69, 70, 72, 73, 74, 75, 77, 78, 79, 81, 82, 83, 85, 86, 87, 89,
-    90, 92, 93, 95, 96, 98, 99, 101, 102, 104, 105, 107, 109, 110, 112, 114,
-    115, 117, 119, 120, 122, 124, 126, 127, 129, 131, 133, 135, 137, 138, 140, 142,
-    144, 146, 148, 150, 152, 154, 156, 158, 160, 162, 164, 167, 169, 171, 173, 175,
-    177, 180, 182, 184, 186, 189, 191, 193, 196, 198, 200, 203, 205, 208, 210, 213,
-    215, 218, 220, 223, 225, 228, 231, 233, 236, 239, 241, 244, 247, 249, 252, 255
-};
-
-
 
 static const uint8_t rainbow_wave_steps = 1;
 KeyboardioScanner controller2(3);
@@ -112,9 +89,6 @@ void effect_rainbow_update() {
     }
     hsv_to_rgb(&rainbow, rainbow_hue, 255, 10);
 
-    // rainbow_out.r = pgm_read_byte(&gamma[rainbow.r]);
-    //  rainbow_out.g = pgm_read_byte(&gamma[rainbow.g]);
-    // rainbow_out.b = pgm_read_byte(&gamma[rainbow.b]);
 
     controller.setAllLEDsTo(rainbow);
 
@@ -135,11 +109,8 @@ void effect_rainbow_wave_update(int direction) {
         }
         hsv_to_rgb(&rainbow, key_hue, 255, 100);
 
-        rainbow_out.r = pgm_read_byte(&gamma[rainbow.r]);
-        rainbow_out.g = pgm_read_byte(&gamma[rainbow.g]);
-        rainbow_out.b = pgm_read_byte(&gamma[rainbow.b]);
 
-        led_set_crgb_at(i, rainbow_out);
+        led_set_crgb_at(i, rainbow);
     }
     rainbow_wave_hue += (rainbow_wave_steps * direction);
     if (rainbow_wave_hue >= 255)          {
